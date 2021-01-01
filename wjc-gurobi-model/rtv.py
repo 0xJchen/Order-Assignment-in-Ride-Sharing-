e9cb55dfca9fc12569fd4a69bb9ff72496b2eb38
PeppaCat <jackywang0404@foxmail.com>
2021-01-02 02:24:19 +0800

    finish testing script

--- wjc-gurobi-model/rtv.py
+++ wjc-gurobi-model/rtv.py
@@ -5,13 +5,17 @@ import re
 from model import *
 from utility import *
 import matplotlib.pyplot as plt
-
-plot_trips=[]
+import greedy as gd
+import pickle
+# from tqdm import trange
+# plot_trips=[]
 # task_count = 2
+import time
 
-def run_task(task_count):
+
+def generate_metadata(task_count):
     requests, trips, vehicles, scores, data = get_task(task_count)
-    plot_trips.append(trips)
+    # plot_trips.append(trips)
     # extract columns from dataframe
     # print(data)
     trip_col = 'Orders='+str(requests)
@@ -66,30 +70,117 @@ def run_task(task_count):
     param['unique_trips'] = unique_trips
     param['vehicles'] = vehicles
 
-    task_time=rtv(tv_graph, rt_graph, tv_cost, param)
-    return task_time
+    task_metadata = {}
+    task_metadata['requests'] = requests
+    task_metadata['trips'] = trips
+    task_metadata['unique_trips'] = unique_trips
+    task_metadata['vehicles'] = vehicles
+
+    task_metadata['tv_graph'] = tv_graph
+    task_metadata['rt_graph'] = rt_graph
+    task_metadata['tv_cost'] = tv_cost
+    task_metadata['task_count']=task_count
+
+    meta_file = "./"+"metadata/"+str(task_count)
+    pickle.dump(task_metadata, open(meta_file, "wb"))
+    return task_metadata
+
+
+def run_one_task(task_count, from_metadata=True, greedy=0):
+
+    if from_metadata:
+        meta_file = "./"+"metadata/"+str(task_count)
+        task_metadata = pickle.load(open(meta_file, 'rb'))
+    else:
+        task_metadata = generate_metadata(task_count)
+
+    # requests = task_metadata['requests']
+    # trips = task_metadata['trips']
+    # unique_trips = task_metadata['unique_trips']
+    # vehicles = task_metadata['vehicles']
+    # tv_graph = task_metadata['tv_graph']
+    # rt_graph = task_metadata['rt_graph']
+    # tv_cost = task_metadata['tv_cost']
+    print("finish loading metadata")
+
+    eps_matrix = None
+    kai_matrix = None
+    gd_cost = None
+    gd_time = None
+    if greedy == 0:  # pure mip
+        mip_time, mip_cost = rtv(task_metadata, False, eps_matrix, kai_matrix)
+    elif greedy == 1:
+        _, eps_matrix, kai_matrix, gd_cost, gd_time = gd.greedy_initial1(
+            task_count)
+        mip_time, mip_cost=rtv(task_metadata, True, eps_matrix, kai_matrix)
+    else:
+        assert(greedy == 2)
+        _, eps_matrix, kai_matrix, gd_cost, gd_time = gd.greedy_initial2(
+            task_count)
+        mip_time, mip_cost = rtv(task_metadata, True, eps_matrix, kai_matrix)
+    return mip_time, mip_cost, gd_cost, gd_time
+
+
+def run_task(task_count, initialization, eps_matrix, kai_matrix,):
+    requests, trips, vehicles, scores, data = get_task(task_count)
+
+    param = {}
+    param['requests'] = requests
+    param['trips'] = trips
+    param['unique_trips'] = unique_trips
+    param['vehicles'] = vehicles
+
+    task_metadata = {}
+    task_metadata['requests'] = requests
+    task_metadata['trips'] = trips
+    task_metadata['unique_trips'] = unique_trips
+    task_metadata['vehicles'] = vehicles
+    task_metadata['tv_graph'] = tv_graph
+    task_metadata['rt_graph'] = rt_graph
+    task_metadata['tv_cost'] = tv_cost
+
+    meta_file = "task1"+str(task_count)
+    pickle.dump(task_metadata, open("meta_file", "wb"))
+
+    if initialization:
+        task_time, objvalue = rtv(
+            tv_graph, rt_graph, tv_cost, param, True, eps_matrix, kai_matrix,)
+    else:
+        task_time, objvalue = rtv(
+            tv_graph, rt_graph, tv_cost, param, False, eps_matrix, kai_matrix,)
+    return task_time, objvalue
+
+
 def task_info(task_count):
     print('*'*20+'TASK: '+str(task_count)+'*'*20)
-if __name__=='__main__':
-    plot_time=[]
-    # task_list=[i for i in range(1,10)]
-    task_list=[i for i in range(118,120)]
-    plt.show()
+
+
+if __name__ == '__main__':
+    st = time.time()
+    pure_mip_time = []
+    pure_mip_value = []
+    greedy_mip_time = []
+    greedy_mip_value = []
+    task_list = [i for i in range(112, 121)]
+    msg("start without initialization")
+    # without initialization
     for i in task_list:
         task_info(i)
-        task_time=run_task(i)
-        plot_time.append(task_time)
-        # print(plot_time)
-    plt.plot(plot_time)
-    plt.show()   
-
-
-        
-#     df = pd.DataFrame({'month': [1, 4, 7, 10],
-#                     'year': [2012, 2014, 2013, 2014],
-#                      'sale': [55, 40, 84, 31]})
-#     s = pd.Series(['a','b','c','d'])
-#     df=df.set_index(s)
-#     p(df)
-#     print('*'*80)
-#     print(df.at['a','month'])
+        task_time, objvalue = run_task(i, False, None, None)
+        pure_mip_time.append(task_time)
+        pure_mip_value.append(objvalue)
+    # with initalization
+    msg("start with initialization")
+    for i in task_list:
+        task_info(i)
+        df_unique_trip, eps_matrix, kai_matrix = gd.greedy_initial(i)
+        task_time, objvalue = run_task(i, True, eps_matrix, kai_matrix)
+        greedy_mip_time.append(task_time)
+        greedy_mip_value.append(objvalue)
+    save_list(pure_mip_time, pure_mip_value, greedy_mip_time, greedy_mip_value)
+    compare_time(pure_mip_time, greedy_mip_time)
+    compare_value(pure_mip_value, greedy_mip_value)
+    print(pure_mip_value, greedy_mip_value)
+    print([a_i - b_i for a_i, b_i in zip(pure_mip_value, greedy_mip_value)])
+    print("finish in {}".format(time.time()-st))
+    # print([a_i - b_i for a_i, b_i in zip(pure_mip_value, greedy_mip_value)])
