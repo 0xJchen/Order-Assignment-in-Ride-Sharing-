c18f160a0dc7403a82ce188a78314f9bb8f4a448
Carlxuzhl <xuzhongling@xuzhonglingdeMacBook-Pro.local>
2021-01-01 23:42:09 +0800

    plot_violate

--- wjc-gurobi-model/plot_script.py
+++ wjc-gurobi-model/plot_script.py
@@ -6,8 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 x = np.zeros(120)
-violate1 = np.zeros(120)
-violate2 = np.zeros(120)
+violate1_1 = np.zeros(120)
+violate1_2 = np.zeros(120)
+violate2_1 = np.zeros(120)
+violate2_2 = np.zeros(120)
 
 infile = open('rt_graph_list','rb')
 rt_graph_list = pickle.load(infile)
@@ -22,24 +24,28 @@ for i in range(120):
 	rt_graph2 = rt_graph_list[2*i+1]
 	for j in range(vehicles):
 		if sum(eps_matrix1[i][j] for i in range(requests)) > 1 :
-			violate1[i] = 1
+			violate1_1[i] = 1
 			print('Constraint violated')
 		if sum(eps_matrix2[i][j] for i in range(requests)) > 1 :
-			violate2[i] = 1
+			violate2_1[i] = 1
 			print('Constraint violated')
 	for k in range(requests):
-		if sum(rt_graph1[k][h]*eps_matrix1[h][c]  for c in range(vehicles) for h in range(unique_trips1)) != 1:
-			violate1[i] = 1
+		if sum(rt_graph1[k][h]*eps_matrix1[h][c] for c in range(vehicles) for h in range(unique_trips1)) != 1:
+			violate1_2[i] = 1
 			print('Constraint violated')
-		if sum(rt_graph2[k][h]*eps_matrix2[h][c]  for c in range(vehicles) for h in range(unique_trips2)) != 1:
-			violate2[i] = 1
+		if sum(rt_graph2[k][h]*eps_matrix2[h][c] for c in range(vehicles) for h in range(unique_trips2)) != 1:
+			violate2_2[i] = 1
 			print('Constraint violated')
 	x[i] = totalCost1 - totalCost2
 
 plt.plot(x)
 plt.savefig('diff.jpg')
 
-outfile1 = open('violate1','wb')
-outfile2 = open('violate2','wb')
-pickle.dump(violate1, outfile1)
-pickle.dump(violate1, outfile2)
+outfile1_1 = open('violate1_1','wb')
+outfile1_2 = open('violate1_2','wb')
+outfile2_1 = open('violate2_1','wb')
+outfile2_2 = open('violate2_2','wb')
+pickle.dump(violate1_1, outfile1_1)
+pickle.dump(violate1_2, outfile1_2)
+pickle.dump(violate2_1, outfile2_1)
+pickle.dump(violate2_2, outfile2_2)
